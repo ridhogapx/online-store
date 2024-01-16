f3472e20301a0e4ba72b5050ea47aa1c25e6ac61
RageNeko26 <rneko2006@gmail.com>
2024-01-16 10:15:46 +0700

    feat: Generate SQLC code from query

--- db/query/product.sql
+++ db/query/product.sql
@@ -3,11 +3,10 @@ INSERT INTO products (
     id,
     category_id,
     name,
-    price,
+    price
 ) VALUES (
-    $1, $2, $3
+    $1, $2, $3, $4
 ) RETURNING *;
 
 -- name: FindProductByCategory :many
-SELECT products.id, categories.name, products.name, products.price, products.created_at FROM products 
-WHERE products.category_id=$1 INNER JOIN categories ON products.category_id=categories.id;
+SELECT products.id, categories.name, products.name, products.price, products.created_at FROM products INNER JOIN categories ON products.category_id=categories.id;
