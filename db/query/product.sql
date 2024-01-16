3f3fb82ad6708b1a61cf0243644d641171b27906
RageNeko26 <rneko2006@gmail.com>
2024-01-16 22:00:58 +0700

    refactor: Separate query into files

--- db/query/product.sql
+++ db/query/product.sql
@@ -17,18 +17,3 @@ INSERT INTO categories (
 ) VALUES (
     $1
 ) RETURNING *;
-
--- name: CreateCart :one
-INSERT into shopping_carts (
-    cart_id,
-    customer_id, 
-    product_id
-) VALUES (
-    $1, $2, $3
-) RETURNING *;
-
--- name: FindCart :many
-SELECT shopping_carts.cart_id, customers.customer_name, products.product_name FROM shopping_carts
-    INNER JOIN customers ON shopping_carts.customer_id=customers.customer_id
-    INNER JOIN products on shopping_carts.product_id=products.product_id
-    WHERE customers.customer_name=$1;
