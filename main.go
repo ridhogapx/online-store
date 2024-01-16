660f372d6daa83337791d1217d151005c2ec41ef
RageNeko26 <rneko2006@gmail.com>
2024-01-16 14:56:12 +0700

    feat: Inject every dependencies

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/RageNeko26/online-store/config"
+	"github.com/RageNeko26/online-store/controller"
+	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	app := fiber.New()
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load environment variable")
+	}
+
+	// Load up environment variable
+	DB_SOURCE := os.Getenv("DB_SOURCE")
+
+	// Initialize configuration
+	q := config.NewDBConnection(DB_SOURCE)
+
+	controller := controller.Setup(app, q)
+	// Initialize route
+	controller.Routes()
+
+	// Internal logging
+	fmt.Println("Server is running")
 
 	app.Listen(":3000")
 }
