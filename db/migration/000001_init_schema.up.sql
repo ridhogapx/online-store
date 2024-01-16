e15144bacd14bfc44aa9a2872f38032ef96daf21
RageNeko26 <rneko2006@gmail.com>
2024-01-16 11:35:33 +0700

    fix: Change primary key name

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,41 +1,41 @@
 CREATE TABLE "products" (
-  "id" varchar PRIMARY KEY NOT NULL,
+  "product_id" varchar PRIMARY KEY NOT NULL,
   "category_id" varchar NOT NULL,
-  "name" varchar NOT NULL,
+  "product_name" varchar NOT NULL,
   "price" bigserial NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "categories" (
-  "id" BIGSERIAL PRIMARY KEY NOT NULL,
-  "name" varchar NOT NULL
+  "category_id" BIGSERIAL PRIMARY KEY NOT NULL,
+  "category_name" varchar NOT NULL
 );
 
 CREATE TABLE "customers" (
-  "id" varchar PRIMARY KEY NOT NULL,
-  "name" varchar NOT NULL,
-  "address" varchar NOT NULL,
+  "customer_id" varchar PRIMARY KEY NOT NULL,
+  "customer_name" varchar NOT NULL,
+  "customer_address" varchar NOT NULL,
   "email" varchar NOT NULL,
   "password" varchar NOT NULL
 );
 
 CREATE TABLE "shopping_carts" (
-  "id" varchar PRIMARY KEY NOT NULL,
+  "cart_id" varchar PRIMARY KEY NOT NULL,
   "customer_id" varchar NOT NULL,
   "product_id" varchar NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transaction_reports" (
-  "id" varchar PRIMARY KEY NOT NULL,
+  "transaction_id" varchar PRIMARY KEY NOT NULL,
   "cart_id" varchar,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("category_id");
 
-ALTER TABLE "shopping_carts" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("id");
+ALTER TABLE "shopping_carts" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id");
 
-ALTER TABLE "shopping_carts" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ALTER TABLE "shopping_carts" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("product_id");
 
-ALTER TABLE "transaction_reports" ADD FOREIGN KEY ("cart_id") REFERENCES "shopping_carts" ("id");
+ALTER TABLE "transaction_reports" ADD FOREIGN KEY ("cart_id") REFERENCES "shopping_carts" ("cart_id");
