d1666262a539b0345e67fd61bdafcb820d8a4e70
RageNeko26 <rneko2006@gmail.com>
2024-01-16 13:31:17 +0700

    test: It should adding data into database

--- db/sqlc/product_test.go
+++ db/sqlc/product_test.go
@@ -4,10 +4,27 @@ import (
 	"context"
 	"fmt"
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 func TestCreateCategory(t *testing.T) {
 	res, err := testQueries.CreateCategory(context.Background(), "electronic")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println("Result:", res)
+}
+
+func TestCreateProduct(t *testing.T) {
+	res, err := testQueries.CreateProduct(context.Background(), CreateProductParams{
+		ProductID:   uuid.New().String(),
+		CategoryID:  2,
+		ProductName: "Iphone 8",
+		Price:       8000000,
+	})
 
 	if err != nil {
 		t.Error(err)
