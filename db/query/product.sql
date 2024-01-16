8ad6a5e69912d1e800583ed14d17019f53dfe3dd
RageNeko26 <rneko2006@gmail.com>
2024-01-16 11:57:07 +0700

    feat: Add query for adding Category

--- db/query/product.sql
+++ db/query/product.sql
@@ -10,3 +10,11 @@ INSERT INTO products (
 
 -- name: FindProductByCategory :many
 SELECT products.product_id, categories.category_name, products.product_name, products.price, products.created_at FROM products INNER JOIN categories ON products.category_id=categories.category_id WHERE products.category_id=$1;
+
+-- name: CreateCategory :one
+INSERT INTO categories (
+    category_id,
+    category_name
+) VALUES (
+    $1, $2
+) RETURNING *;
