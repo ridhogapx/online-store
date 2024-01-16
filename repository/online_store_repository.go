6567ed4bc7ab4cf704573a6f0b8fa50820d55710
RageNeko26 <rneko2006@gmail.com>
2024-01-16 10:44:50 +0700

    refactor: Change interface repository name

--- repository/online_store_repository.go
+++ repository/online_store_repository.go
@@ -2,7 +2,7 @@ package repository
 
 import "github.com/RageNeko26/online-store/entity"
 
-type ProductRepository interface {
+type OnlineStoreRepository interface {
 	CreateProduct(*entity.ProductEntity) error
 	FindProductByCategory(id string) (*entity.ProductEntity, error)
 }
