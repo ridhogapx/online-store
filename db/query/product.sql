48d9b655b83c09dd5f7700a4ecc27679b50862e5
RageNeko26 <rneko2006@gmail.com>
2024-01-16 21:48:06 +0700

    feat: Add query for Finding Cart item

--- db/query/product.sql
+++ db/query/product.sql
@@ -26,3 +26,9 @@ INSERT into shopping_carts (
 ) VALUES (
     $1, $2, $3
 ) RETURNING *;
+
+-- name: FindCart :many
+SELECT shopping_carts.cart_id, customers.customer_name, products.product_name FROM shopping_carts
+    INNER JOIN customers ON shopping_carts.customer_id=customers.customer_id
+    INNER JOIN products on shopping_carts.product_id=products.product_id
+    WHERE customers.customer_name=$1;
