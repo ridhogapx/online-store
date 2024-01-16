2eb9beace48b68f7a502e7ba682e3b73c3f0fcec
RageNeko26 <rneko2006@gmail.com>
2024-01-16 12:45:49 +0700

    refactor: Remove repository pattern

--- repository/online_store_repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/RageNeko26/online-store/entity"
-
-type OnlineStoreRepository interface {
-	CreateProduct(*entity.ProductEntity) error
-	FindProductByCategory(id string) (*entity.ProductEntity, error)
-}
