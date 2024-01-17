b590fe622d1e44161db8111e056c1ff9250be171
RageNeko26 <rneko2006@gmail.com>
2024-01-17 11:35:24 +0700

    feat: Add controller for login

--- controller/web.go
+++ controller/web.go
@@ -11,3 +11,12 @@ type RegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
