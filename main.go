161cd9e9397295c14d033359a0fbeed7711fbb10
RageNeko26 <rneko2006@gmail.com>
2024-01-17 22:47:32 +0700

    fix: Remove unused module

--- main.go
+++ main.go
@@ -2,23 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/RageNeko26/online-store/config"
 	"github.com/RageNeko26/online-store/controller"
 	"github.com/gofiber/fiber"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
-
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Failed to load environment variable")
-	}
 
 	// Load up environment variable
 	DB_SOURCE := os.Getenv("DB_SOURCE")
